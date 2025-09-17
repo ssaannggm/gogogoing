1ba77d5518b410ee4cd702fb1db6c5cf6990ca87
ssaannggm <tkdals22222@gmail.com>
2025-09-17 11:45:04 +0900

    Revert "123123"
    This reverts commit 4319359694819949fcb8aff7f45d900563d81d26.

--- Assets/Game/Scripts/UI/InventoryListPanel.cs
+++ Assets/Game/Scripts/UI/InventoryListPanel.cs
@@ -1,4 +1,4 @@
-﻿// Assets/Game/Scripts/UI/InventoryListPanel.cs
+﻿// InventoryListPanel.cs - 전체 코드
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Game.Services;
@@ -13,11 +13,14 @@ namespace Game.UI
         [Header("UI 설정")]
         [SerializeField] private Transform _contentParent;
 
-        private readonly List<GameObject> _spawnedIcons = new List<GameObject>();
+        private List<GameObject> _spawnedIcons = new List<GameObject>();
 
         public void Refresh(InventoryPartyMode controller, IReadOnlyList<string> itemIds, bool isDraggable)
         {
-            foreach (var icon in _spawnedIcons) if (icon) Destroy(icon);
+            foreach (var icon in _spawnedIcons)
+            {
+                if (icon) Destroy(icon);
+            }
             _spawnedIcons.Clear();
 
             var dataCatalog = GameManager.I?.Data;
@@ -29,7 +32,7 @@ namespace Game.UI
                 var itemSO = dataCatalog.GetItemById(itemId);
                 if (itemSO != null)
                 {
-                    var iconGO = Object.Instantiate(itemIconPrefab, _contentParent);
+                    var iconGO = Instantiate(itemIconPrefab, _contentParent);
                     var itemIcon = iconGO.GetComponent<ItemIconUI>();
                     itemIcon.Setup(itemSO, controller, isDraggable);
                     _spawnedIcons.Add(iconGO);
@@ -37,20 +40,21 @@ namespace Game.UI
             }
         }
 
-        // InventoryListPanel.OnDrop
         public void OnDrop(PointerEventData eventData)
         {
-            var droppedIcon = eventData.pointerDrag ? eventData.pointerDrag.GetComponent<ItemIconUI>() : null;
-            Debug.Log($"[INV] OnDrop pointerDrag={eventData.pointerDrag?.name} icon={droppedIcon?.ItemData?.name}");
+            var droppedIcon = eventData.pointerDrag.GetComponent<ItemIconUI>();
             if (droppedIcon == null) return;
 
+            // [✨ 핵심 수정]
+            // 드롭된 아이콘이 장비 슬롯에서 온 '유령'이고,
+            // 그 유령 아이콘이 컨트롤러(controller)에 대한 참조를 가지고 있을 경우에만 장비 해제 처리
             if (droppedIcon.IsGhost && droppedIcon.SourceSlot != null && droppedIcon.Controller != null)
             {
-                var source = droppedIcon.SourceSlot;
-                droppedIcon.Controller.HandleUnequipRequest(source.CharacterIndex, source.slotType);
-                droppedIcon.Controller.NotifyDropSucceeded();
+                var sourceSlot = droppedIcon.SourceSlot;
+                // GetComponentInParent 대신, 유령 아이콘이 직접 들고 있는 컨트롤러 참조를 사용합니다.
+                // 이것이 훨씬 안정적입니다.
+                droppedIcon.Controller.HandleUnequipRequest(sourceSlot.CharacterIndex, sourceSlot.slotType);
             }
         }
-
     }
 }
